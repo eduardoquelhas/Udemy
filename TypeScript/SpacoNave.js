1c80e4e36ac45c70d999edf6e1e30afa13064717
eduardoquelhas <eduardoquelhas@yahoo.com.br>
2017-12-09 19:02:58 -0200

    Importando uma Biblioteca em JS

--- TypeScript/SpacoNave.js
+++ TypeScript/SpacoNave.js
@@ -2,9 +2,13 @@
 exports.__esModule = true;
 var NaveBase_1 = require("./NaveBase");
 var ComecarLuta_1 = require("./ComecarLuta");
+var _ = require("lodash");
+//Comando abaixo Criado pelo LoDash
+console.log(_.pad("Examplos de TypeScript", 80, "*"));
 var SpaccoNave = new NaveBase_1.NaveEspacial('HyperDrive');
 SpaccoNave.puloParaHyperSpaco();
 var falcon = new ComecarLuta_1.MillenniumFlacon();
 falcon.puloParaHyperSpaco();
 var boaParaCarga = function (espaconave) { return espaconave.numeroContatiner > 2; };
+console.log(_.pad("Falcon", 60, ":-)"));
 console.log("A Falcon \u00E9 boa para Caraga ? " + (boaParaCarga(falcon) ? 'Sim muito Boa' : 'Não poucos Containers') + " ");
--- TypeScript/SpacoNave.ts
+++ TypeScript/SpacoNave.ts
@@ -2,6 +2,14 @@
 import {NaveEspacial, NaveCarga } from './NaveBase'
 import {MillenniumFlacon} from './ComecarLuta'
 
+import * as _ from 'lodash'
+
+//Comando abaixo Criado pelo LoDash (  _.pad  )
+//npm install --save lodash@4.14
+//npm install --save-dev @types/lodash@4.14
+
+console.log( _.pad( "Examplos de TypeScript",80,"*") )
+
 let SpaccoNave = new NaveEspacial('HyperDrive')
 SpaccoNave.puloParaHyperSpaco()
 
@@ -10,4 +18,5 @@ falcon.puloParaHyperSpaco()
 
 let boaParaCarga = (espaconave:NaveCarga) => espaconave.numeroContatiner > 2
 
+console.log( _.pad( "Falcon",60,":-)") )
 console.log(`A Falcon é boa para Caraga ? ${boaParaCarga (falcon) ? 'Sim muito Boa':'Não poucos Containers'} `)
