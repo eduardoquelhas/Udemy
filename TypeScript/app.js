88c885dec630d898fe8138b33891028329089079
eduardoquelhas <eduardoquelhas@yahoo.com.br>
2017-12-09 15:35:35 -0200

    Atualização com Funcoes

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -18,3 +18,13 @@ var shoteStrun = function (nrEntrada) {
 var nrSaida;
 nrSaida = 12;
 console.log("O Numero " + nrSaida + " é maior que 12 Verdade = " + shoteStrun(nrSaida));
+var distancia = 13;
+console.log("A Velocidade de " + distancia + " Km \u00E9 suficiente para bater uma aeronave? " + (shoteStrun(distancia) ? 'Claro que Sim' : 'Não Consegue') + " ");
+var Chamada = function (name) { return console.log("Qual o seu nome, " + name + "?"); };
+Chamada('dudu');
+function inc(speed, inc) {
+    if (inc === void 0) { inc = 1; }
+    return speed + inc;
+}
+console.log("inc (5,1) = " + inc(5, 1));
+console.log("inc (5) = " + inc(5));
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -23,8 +23,21 @@ let  shoteStrun = function (nrEntrada:number):boolean {
     }
 }
 
-
 let nrSaida:number
 nrSaida = 12
 
 console.log("O Numero " + nrSaida +" é maior que 12 Verdade = " + shoteStrun(nrSaida) )
+
+let distancia= 13
+console.log(`A Velocidade de ${distancia} Km é suficiente para bater uma aeronave? ${shoteStrun(distancia) ? 'Claro que Sim': 'Não Consegue'} `)
+
+let Chamada = (name: string) => console.log(`Qual o seu nome, ${name}?`)
+Chamada('dudu')
+
+function inc( speed: number , inc: number = 1) :number {
+
+    return speed + inc
+}
+
+console.log(`inc (5,1) = ${inc(5,1)}`)
+console.log(`inc (5) = ${inc(5)}`)
